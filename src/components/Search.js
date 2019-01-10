24027387414d42cf0181a6f6e0bd68cce9df6b8a
Annapurna13 <yoavgecht@gmail.com>
2019-01-10 20:36:46 +0200

    updated search input placeholder

--- src/components/Search.js
+++ src/components/Search.js
@@ -8,7 +8,7 @@ class Search extends Component {
                      <form className="form-inline mb-3" onSubmit={(e) => this.props.getVenues(e)}>
                         <div className="form-group mb-2">
                             <label htmlFor="inputPassword2" className="sr-only">Password</label>
-                            <input type="text" className="form-control" id="search-input" placeholder="Search" value={this.props.searchTerm} onChange={(e) => this.props.handleSearchInput(e)} />
+                            <input type="text" className="form-control" id="search-input" placeholder="Search a city" value={this.props.searchTerm} onChange={(e) => this.props.handleSearchInput(e)} />
                         </div>
                         <button type="submit" className="btn btn-primary ml-3 mb-2 search-btn" disabled={this.props.searchTerm.length < 1}>Search</button>
                     </form>
