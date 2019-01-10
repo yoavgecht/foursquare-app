54b21557bbc1aecadb64fec0f60ff386f74cc5c4
Annapurna13 <yoavgecht@gmail.com>
2019-01-10 14:47:34 +0200

    Updates

---
+++ precache-manifest.56730b9cc8c485c556d5df957672663b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e235e552fb731531bee6",
+    "url": "/foursquare-app/static/css/main.6130fef5.chunk.css"
+  },
+  {
+    "revision": "e235e552fb731531bee6",
+    "url": "/foursquare-app/static/js/main.e235e552.chunk.js"
+  },
+  {
+    "revision": "34b3d0a6bd3ea04f7d5d",
+    "url": "/foursquare-app/static/css/1.2d0ec906.chunk.css"
+  },
+  {
+    "revision": "34b3d0a6bd3ea04f7d5d",
+    "url": "/foursquare-app/static/js/1.34b3d0a6.chunk.js"
+  },
+  {
+    "revision": "9852f6309d514c85258c",
+    "url": "/foursquare-app/static/js/runtime~main.9852f630.js"
+  },
+  {
+    "revision": "6e2ff27755619d50bdfa2a8d6227f0c0",
+    "url": "/foursquare-app/index.html"
+  }
+];
